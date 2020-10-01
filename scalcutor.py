89dd5fc58ba8b1d2c228feb0fa7ecc5d21cfd0ff
Pranav <72198941+pranav102001@users.noreply.github.com>
2020-10-01 19:04:28 +0530

    Added a new function(exponent) in your calculator.

--- scalcutor.py
+++ scalcutor.py
@@ -14,11 +14,16 @@ def multiply(x, y):
 def divide(x, y):
    return x / y
 
+# This function gives the exponent of two numbers
+def exponent(x, y):
+   return x**y
+
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Exponent")
 
 # Take input from the user 
 choice = input("Enter choice(1/2/3/4):")
@@ -37,5 +42,9 @@ elif choice == '3':
 
 elif choice == '4':
    print(num1,"/",num2,"=", divide(num1,num2))
+   
+elif choice == '5':
+   print(num1,"power",num2,"=", exponent(num1,num2))
+   
 else:
    print("Invalid input")
